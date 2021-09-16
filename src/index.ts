b6efaee4a59a490f0621c4a71eb5a860972bf637
Charles Lowell <cowboyd@frontside.io>
2021-09-16 18:28:11 +0300

    make ESM module the default

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-import * as symbols from './random'
-
 import random from './random'
 export * from './random'
 export default random
-
-Object.assign(random, symbols)
-
-// TODO: figure out tsconfig settings to remove this line
-// (it also appears in the ESM version which is not what we want)
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-module.exports = random
