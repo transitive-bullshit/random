bc16d295834ad7f01890ee2392c0a0436633e77f
Travis Fischer <fisch0920@gmail.com>
2024-11-03 21:55:53 -0600

    chore: simplify deps

--- src/seed.test.ts
+++ src/seed.test.ts
@@ -7,7 +7,7 @@ test('random.clone with seedrandom rng is consistent', () => {
   const r = random.clone(seedrandom('ZjExZDczNWQxY2NlZjUzYmRiZWU0ZGIz'))
   const d = r.uniform()
   const o: number[] = []
-  for (let i = 0; i < 100; ++i) {
+  for (let i = 0; i < 10; ++i) {
     const v = d()
     o.push(v)
   }
@@ -19,7 +19,7 @@ test('random.clone with string seed is consistent', () => {
   const r = random.clone('MDUzNzM5ZGQwYWZlNWIzNjI5MDYwNWNm')
   const d = r.normal()
   const o: number[] = []
-  for (let i = 0; i < 100; ++i) {
+  for (let i = 0; i < 10; ++i) {
     const v = d()
     o.push(v)
   }
