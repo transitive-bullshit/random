7ccca3bfd2e32bf8ee0c73a5d5c7f0018e107103
fredcallaway <fredc@llaway.com>
2025-07-11 23:10:01 -0400

    support p=1 for bernoulli

--- src/distributions/bernoulli.ts
+++ src/distributions/bernoulli.ts
@@ -2,9 +2,9 @@ import type { Random } from '../random'
 import { numberValidator } from '../validation'
 
 export function bernoulli(random: Random, p = 0.5) {
-  numberValidator(p).greaterThanOrEqual(0).lessThan(1)
+  numberValidator(p).greaterThanOrEqual(0).lessThanOrEqual(1)
 
   return () => {
-    return Math.floor(random.next() + p)
+    return Math.min(1, Math.floor(random.next() + p))
   }
 }
